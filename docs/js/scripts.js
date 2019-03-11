5e2b6d454bd154661c337e6b0e6a2302adc872c2
Quentin Gibson <qgibson3@students.kennesaw.edu>
2019-03-11 19:16:23 -0400

    add modals

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -9,89 +9,93 @@
 
 (function ($) {
 
-	$.fn.kellyMagnificPopup = function (opts) {
+  $.fn.kellyMagnificPopup = function (opts) {
 
-		var $self = $(this),
-			options = $.extend({
-				delegate   : '.media',
-				type       : 'image',
-				tLoading   : '<div class="dots">\
+    var $self = $(this),
+      options = $.extend({
+        delegate: '.media',
+        type: 'image',
+        tLoading: '<div class="dots">\
 							<div class="dot active"></div>\
 							<div class="dot active"></div>\
 							<div class="dot active"></div>\
 							<div class="dot active"></div>\
 						</div>',
-				mainClass  : 'mfp-img-mobile',
-				gallery    : {
-					enabled           : true,
-					navigateByImgClick: true
-				},
-				image      : {
-					tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-				},
-				closeMarkup: '<button title="%title%" type="button" class="mfp-close"></button>',
-				callbacks  : {
-					markupParse      : function (item) {
-					},
-					imageLoadComplete: function () {
-						var $container = $('.mfp-container');
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+          enabled: true,
+          navigateByImgClick: true
+        },
+        image: {
+          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        },
+        closeMarkup: '<button title="%title%" type="button" class="mfp-close"></button>',
+        callbacks: {
+          markupParse: function (item) {
+          },
+          imageLoadComplete: function () {
+            var $container = $('.mfp-container')
 
-						$container.addClass('load-done');
-						setTimeout(function () {
-							$container.addClass('load-transition');
-						}, 50);
-					},
-					change           : function () {
-						var $container = $('.mfp-container');
-						$container.removeClass('load-done load-transition');
-					}
+            $container.addClass('load-done')
+            setTimeout(function () {
+              $container.addClass('load-transition')
+            }, 50)
+          },
+          change: function () {
+            var $container = $('.mfp-container')
+            $container.removeClass('load-done load-transition')
+          }
 
-				}
-			}, $self.data(), opts);
-		$('.media', $self).each( function () {
-			var href = $(this).data('url');
+        }
+      }, $self.data(), opts)
+    $('.media', $self).each(function () {
+      var href = $(this).data('url')
 
-			if (href && href !== '') {
-				$(this).attr('href', href);
-			}
+      if (href && href !== '') {
+        $(this).attr('href', href)
+      }
 
-		});
-		$self.magnificPopup(options);
-	};
+    })
+    $self.magnificPopup(options)
+  }
 
-	$(document).ready(function () {
+  $(document).ready(function () {
 
-		var $header = $('.header'),
-			$search = $('.fa-search', $header);
+    var $header = $('.header'),
+      $search = $('.fa-search', $header)
 
-		// 3. Menu Mobile
-		var $btnMenu = $('.menu-mobile'),
-			$hideMenu = $('.hide-menu');
+    // 3. Menu Mobile
+    var $btnMenu = $('.menu-mobile'),
+      $hideMenu = $('.hide-menu')
 
-		$btnMenu.on('click', function () {
-			$header.toggleClass('active');
+    $btnMenu.on('click', function () {
+      $header.toggleClass('active')
 
-			if ($header.hasClass('active')) {
-				$hideMenu.addClass('active');
-			}
-			else {
-				$hideMenu.removeClass('active');
-			}
-		});
-		$hideMenu.on('click', function () {
-			$header.removeClass('active');
-			$hideMenu.removeClass('active');
-		});
+      if ($header.hasClass('active')) {
+        $hideMenu.addClass('active')
+      } else {
+        $hideMenu.removeClass('active')
+      }
+    })
+    $hideMenu.on('click', function () {
+      $header.removeClass('active')
+      $hideMenu.removeClass('active')
+    })
 
-		$('.menu-item-has-children', '.main-menu').on('click', ' > a', function (e) {
-			var ww = $(window).width();
+    $('.menu-item-has-children', '.main-menu').on('click', ' > a', function (e) {
+      var ww = $(window).width()
 
-			if (ww <=991) {
-				var $parent = $(e.target).closest('.menu-item-has-children');
-				e.preventDefault();
-				$('>.sub-menu', $parent).slideToggle(400);
-			}
-		});
-	});
+      if (ww <= 991) {
+        var $parent = $(e.target).closest('.menu-item-has-children')
+        e.preventDefault()
+        $('>.sub-menu', $parent).slideToggle(400)
+      }
+    })
+  })
+})(jQuery)
 
-})(jQuery);
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var options = {}
+  var instances = M.Materialbox.init(elems, options);
+});
